86d11557754e0cf95e394000acfd8a92391f4af3
Sabrina Campos <sabrinacampos@outlook.com.br>
2025-11-22 11:20:55 -0400

    Esqueleto da tela de login feita

---
+++ app/src/main/java/com/example/muralmobile/activities/Login.java
@@ -0,0 +1,18 @@
+package com.example.muralmobile.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.muralmobile.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+
+}

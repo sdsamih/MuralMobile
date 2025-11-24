586ae532fa7b7576b4774360db21de8ff9d7b97d
pc-brina <sabrinacampos003@gmail.com>
2025-11-24 19:28:31 -0400

    Esqueleto da tela de Login e Cadastro

--- app/src/main/java/com/example/muralmobile/activities/Login.java
+++ app/src/main/java/com/example/muralmobile/activities/Login.java
@@ -1,6 +1,10 @@
 package com.example.muralmobile.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+        Button signUp = findViewById(R.id.btnSignup);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

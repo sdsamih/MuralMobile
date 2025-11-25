f636d90215992ee7b0946d2c129d4bfa97a4224b
mand <amandabarrosmatoss@gmail.com>
2025-11-24 21:42:29 -0400

    refactor: change login and signup design

---
+++ app/src/main/java/com/example/muralmobile/activities/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.muralmobile.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.muralmobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button button_create;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button_create = findViewById(R.id.button_create);
+        button_create.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+        });
+    }
+}
